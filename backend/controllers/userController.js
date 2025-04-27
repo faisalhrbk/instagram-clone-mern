6373dd6d96a39625ea3b92f47699d979269f9e71
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-27 18:57:40 +0500

    created register ,login and logout  functions in user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,100 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+	try {
+		const { username, email, password } = req.body;
+		if (!username || !email || !password) {
+			return res.status(401).json({
+				message: "Something is missing please Check",
+				success: false,
+			});
+		}
+		const user = await User.findOne({ email });
+		if (user) {
+			return res.status(401).json({
+				message: "Try different Email",
+				success: false,
+			});
+		}
+		const hashedPassword = await bcrypt(password, 10);
+		User.create({
+			username,
+			email,
+			hashedPassword,
+		});
+		return res.status(201).json({
+			message: "User Register Successfully",
+			success: true,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		if (!email || !password) {
+			return res.status(401).json({
+				message: "Something is missing Email or Password",
+				success: false,
+			});
+		}
+		let user = await findOne({ email });
+		if (!email) {
+			return res.status(401).json({
+				message: "Email doesnt exist in database",
+				success: false,
+			});
+		}
+		const isPasswordMatch = await bcrypt.compare(password, user.password);
+		if (!isPasswordMatch) {
+			return res.status(401).json({
+				message: "password is incorrect",
+				success: false,
+			});
+		}
+
+		const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+			expiresIn: "1d",
+		});
+		user = {
+			_id: user._id,
+			username: user.username,
+			email: user.email,
+			profilePicture: user.profilePicture,
+			bio: user.bio,
+			followers: user.followers,
+			following: user.following,
+			posts: user.posts,
+		};
+		return res
+			.cookie("token", token, {
+				httpOnly: true,
+				sameSite: "strict",
+				maxAge: 1 * 24 * 60 * 60 * 1000,
+			})
+			.status(200)
+			.json({
+				message: `welcome back ${user.username}`,
+				success: true,
+				user,
+			});
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const logout = async (_, res) => {
+	try {
+		return res.clearCookie("token", { httpOnly: true }).json({
+			message: "Logout Successfully",
+			success: true,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+};
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
-	email: { type: String, required: true, unique: true },
-	password: {
-		type: String,
-		required: true,
+const userSchema = new mongoose.Schema(
+	{
+		username: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		password: {
+			type: String,
+			required: true,
+		},
+		profilePicture: { type: String, default: "" },
+		bio: { type: String, default: "" },
+		gender: { type: String, enum: ["male", "female", "other"] },
+		followers: [{ type: mongoose.Schema.Types.objectId, ref: "User" }],
+		following: [{ type: mongoose.Schema.Types.objectId, ref: "User" }],
+		posts: [{ type: mongoose.Schema.Types.objectId, ref: "Post" }],
+		bookmarks: [{ type: mongoose.Schema.Types.objectId, ref: "Post" }],
 	},
-	profilePicture: { type: String, default: "" },
-	bio: { type: String, default: "" },
-	gender: { type: String, enum: ["male", "female", "other"] },
-	followers: [{ type: mongoose.Schema.Types.objectId, ref: "User" }],
-	following: [{ type: mongoose.Schema.Types.objectId, ref: "User" }],
-    'posts':[{type:mongoose.Schema.Types.objectId, ref:'Post'}],
-    'bookmarks': [{type:mongoose.Schema.Types.objectId, ref:"Post"}]
+	{ timestamps: true }
+);
 
-}, {timestamps:true});
-
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
