5337bb1b28b58e0037373091424f0b5cef47fc78
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-27 16:45:22 +0500

    server running successfullya and all packages runs successfully

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,26 @@
 //Core Modules
 // External Modules
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { cors } from 'cors';
 //Local Modules
 
 //middlewares
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
+	origin: "http://localhost:5173",
+	credentials: true,
 };
 app.use(cors(corsOptions));
 
-
-
+//routes go here
+app.get("/", (req, res, next) => {
+	res.send("hello world");
+});
 const PORT = 3000;
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port http://localhost:${PORT}`);
 });
