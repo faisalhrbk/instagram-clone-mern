bffc7ec19b6bd8cfbf71f0bfa95f8dc74a112b23
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-28 17:10:11 +0500

    created getMessages  method in controller

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,4 +1,5 @@
 import Conversation from "../models/Conversation.js";
+import { Conversation } from ".";
 export const sendMessage = async (req, res) => {
 	try {
 		const senderId = req.id;
@@ -21,13 +22,42 @@ export const sendMessage = async (req, res) => {
 		if (newMessage) conversation.newMessage.push(newMessage._id);
 
 		await Promise.all([conversation.save(), newMessage.save()]);
+		//implementing socket io after front end is created
 
-
+		return res.status(201).json({
+			success: true,
+			newMessage,
+		});
 	} catch (err) {
 		console.error(err);
 		return res.status(500).json({
 			message: "Internal Server Error!",
 			success: false,
 		});
+	}
+};
+
+export const getMessage = async (req, res) => {
+	try {
+		const senderId = req.id;
+		const receiverId = req.params.id;
+
+		const conversation = await Conversation.find({
+			participants: { $all: [senderId, receiverId] },
+		});
+
+		if (!conversation) {
+			return res.status(200).json({
+				success: true,
+				messages: [],
+			});
+		}
+
+		return res.status(200).json({
+			success: true,
+			messages: conversation?.messages,
+		});
+	} catch (error) {
+		console.error(error);
 	}
 };
