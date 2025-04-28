052b0b9a3dd67210f1897b89ee31029d9203ade7
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-28 14:09:17 +0500

    created post logic

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,4 +1,7 @@
 import sharp from "sharp";
+import cloudinary from "../utils/cloudinary.js";
+import Post from "../models/Post.js";
+import User from "../models/User.js";
 export const addNewPost = async (req, res) => {
 	try {
 		const { caption } = req.body;
@@ -18,7 +21,27 @@ export const addNewPost = async (req, res) => {
 			})
 			.toFormat("jpeg", { quality: "80" })
 			.toBuffer();
-             
+		//buffer to data uri
+		const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString(
+			"base64"
+		)}`;
+		const cloudResponse = await cloudinary.uploader.upload(fileUri);
+		const post = await Post.create({
+			caption,
+			image: cloudResponse.secure_url,
+			author: authorId,
+		});
+		const user = await User.findById(authorId);
+		if (user) {
+			user.posts.push(post._id);
+			await user.save();
+		}
+		await post.populate({ path: "author", select: "-password" });
+		return res.status(201).json({
+			message: "new post Added",
+			success: true,
+			post,
+		});
 	} catch (err) {
 		return res.status(500).json({
 			message: "Internal Server Error",
