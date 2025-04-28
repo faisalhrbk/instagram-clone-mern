5349c579294f4ae7e53d915ae7735bb1ec4a3d53
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-28 14:24:36 +0500

    created get all posts controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -49,3 +49,26 @@ export const addNewPost = async (req, res) => {
 		});
 	}
 };
+export const getAllPost = async (req, res) => {
+	try {
+		const posts = await Post.find()
+			.sort({ createdAt: -1 })
+			.populate({ path: "author", select: "username profilePicture" })
+			.populate({
+				path: "comments",
+				sort: { createdAt: -1 },
+				populate: { path: "author", select: "username profilePicture" },
+			});
+
+		return res.status(200).json({
+			message: "all posts have been fetched here you goo",
+			posts,
+			success: true,
+		});
+	} catch (err) {
+		return res.status(500).json({
+			message: " Internal Server Error!",
+			success: false,
+		});
+	}
+};
